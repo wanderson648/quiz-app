f3b00b0336c2a0e4a6494b2ff67b411f6abda8e6
wanderson648 <testepedroca@gmail.com>
2022-09-21 23:02:59 -0300

    setting up the question UI connection

--- app/src/main/java/com/example/myquizapp/QuizQuestionActivity.kt
+++ app/src/main/java/com/example/myquizapp/QuizQuestionActivity.kt
@@ -3,17 +3,58 @@ package com.example.myquizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+import java.util.ArrayList
 
 class QuizQuestionActivity : AppCompatActivity() {
+
+    private var progressBar: ProgressBar? = null
+    private var tvProgress: TextView? = null
+    private var tvQuestion: TextView? = null
+    private var ivImage: ImageView? = null
+
+    private var tvOptionOne: TextView? = null
+    private var tvOptionTwo: TextView? = null
+    private var tvOptionThree: TextView? = null
+    private var tvOptionFour: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_question)
 
+        progressBar = findViewById(R.id.progressBar)
+        tvProgress = findViewById(R.id.tv_progress)
+        tvQuestion = findViewById(R.id.tv_question)
+        ivImage = findViewById(R.id.iv_image)
+
+        tvOptionOne = findViewById(R.id.tv_option_one)
+        tvOptionTwo = findViewById(R.id.tv_option_two)
+        tvOptionThree = findViewById(R.id.tv_option_three)
+        tvOptionFour = findViewById(R.id.tv_option_four)
+
+
         val questionsList = Constants.getQuestions()
         Log.i("questionList", "QuestionsList size is: ${questionsList.size}")
 
+
+        for(i in questionsList) {
+            Log.e("Questions", i.questions)
+        }
+
+
+        val currentPosition = 1
+        val question : Question = questionsList[currentPosition - 1]
+        progressBar?.progress = currentPosition
+        ivImage?.setImageResource(question.image)
+        tvProgress?.text = "$currentPosition/${progressBar?.max}"
+        tvQuestion?.text = question.questions
+        tvOptionOne?.text = question.optionOne
+        tvOptionTwo?.text = question.optionTwo
+        tvOptionThree?.text = question.optionThree
+        tvOptionFour?.text = question.optionFour
+
     }
+
 }
